d0523384eeaee8bc253bab8b9c54f7fa12d3b013
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-05-27 09:32:50 +0300

    Create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var carFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("The user flipped " + cardsInPlay[0]);
+console.log("The user flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!");
+	else
+		alert("Sorry, try again.");
+}
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
